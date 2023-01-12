6dea89a872689e2f56cad48feb3b8ebd8561a484
Yusuf Celik <302801376@student.rocmondriaan.nl>
2023-01-12 21:34:44 +0100

    uitschrijven werkt nog niet sql erin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Lesson;
 use App\Entity\UserLesson;
+use App\Form\DeleteLessonType;
+use App\Form\DeleteUserLessonType;
 use App\Form\LessonSigningType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -24,12 +26,23 @@ class UserController extends AbstractController
         $current = new \DateTime($date);
         $lessons = $entityManager->getRepository(Lesson::class)->findBy(['date' => $current]);
 
+//        $user = $this->getUser();
+//        $lesson = $entityManager->getRepository(Lesson::class)->find($lessons);
+//        $userLessonUser = $entityManager->getRepository(UserLesson::class)->findBy(['user' => $user]);
+//        $userLessonLesson = $entityManager->getRepository(UserLesson::class)->findBy(['lesson' => $lesson]);
+//        if (count($userLessonLesson) >= 1 && count($userLessonUser) >= 1) {
+//            $isSignedIn = true;
+//        } else {
+//            $isSignedIn = false;
+//        }
+
         return $this->render('user/lessons.html.twig', [
             'lessons' => $lessons,
+//            'isSignedIn' => $isSignedIn,
         ]);
     }
 
-    #[Route('/lesson/{id}', name: 'app_lesson')]
+    #[Route('/lesson/signin/{id}', name: 'app_lesson_signing')]
     public function lessonSigning($id, EntityManagerInterface $entityManager, Request $request): Response
     {
         $user = $this->getUser();
@@ -51,6 +64,25 @@ class UserController extends AbstractController
             $entityManager->persist($userLesson);
             $entityManager->flush();
         }
+        return $this->redirectToRoute('app_lessons', ['date' => date('Y-m-d')]);
+    }
+
+    #[Route('/lesson/signout/{id}', name: 'app_lesson_signing_out')]
+    public function lessonUnsigning($id, EntityManagerInterface $entityManager, Request $request): Response
+    {
+//        $userLesson = $entityManager->getRepository(UserLesson::class)->findBy(['lesson' => $id]);
+//
+//        foreach ($userLesson as $qwe) {
+//            $qwe->getUser()->getId();
+//        dd($qwe);
+//        }
+//        $userLesson = array_filter($userLesson, 'user');
+//        dd($userLesson);
+//        $form = $this->createForm(DeleteUserLessonType::class, $userLesson);
+//        $form->handleRequest($request);
+//
+//        $entityManager->remove($userLesson);
+//        $entityManager->flush();
         return $this->redirectToRoute('app_lessons', ['date' => date('Y-m-d')]);
     }
 }
