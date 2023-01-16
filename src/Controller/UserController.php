ff95ad68b44ee6aee806adb4abe070f461b1f912
Yusuf Celik <302801376@student.rocmondriaan.nl>
2023-01-16 17:31:50 +0100

    lessons kan in en uitschrijven maar niet helemaal wat ik wil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Lesson;
 use App\Entity\UserLesson;
-use App\Form\DeleteLessonType;
 use App\Form\DeleteUserLessonType;
 use App\Form\LessonSigningType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -47,7 +45,6 @@ class UserController extends AbstractController
     {
         $user = $this->getUser();
         $lesson = $entityManager->getRepository(Lesson::class)->find($id);
-        $userLessons = $entityManager->getRepository(UserLesson::class)->findAll();
 
         $userLessonUser = $entityManager->getRepository(UserLesson::class)->findBy(['user' => $user]);
         $userLessonLesson = $entityManager->getRepository(UserLesson::class)->findBy(['lesson' => $lesson]);
@@ -70,19 +67,22 @@ class UserController extends AbstractController
     #[Route('/lesson/signout/{id}', name: 'app_lesson_signing_out')]
     public function lessonUnsigning($id, EntityManagerInterface $entityManager, Request $request): Response
     {
-//        $userLesson = $entityManager->getRepository(UserLesson::class)->findBy(['lesson' => $id]);
-//
-//        foreach ($userLesson as $qwe) {
-//            $qwe->getUser()->getId();
-//        dd($qwe);
-//        }
-//        $userLesson = array_filter($userLesson, 'user');
-//        dd($userLesson);
-//        $form = $this->createForm(DeleteUserLessonType::class, $userLesson);
-//        $form->handleRequest($request);
-//
-//        $entityManager->remove($userLesson);
-//        $entityManager->flush();
+        $user = $this->getUser();
+        $lesson = $entityManager->getRepository(Lesson::class)->find($id);
+
+        $userLessonUser = $entityManager->getRepository(UserLesson::class)->findBy(['user' => $user]);
+        $userLessonLesson = $entityManager->getRepository(UserLesson::class)->findBy(['lesson' => $lesson]);
+
+        $userLesson = $entityManager->getRepository(UserLesson::class)->findOneBy(['lesson' => $id]);
+        if ($userLessonLesson == [] || $userLessonUser == []) {
+            return $this->redirectToRoute('app_lessons', ['date' => date('Y-m-d')]);
+        } else {
+            $form = $this->createForm(DeleteUserLessonType::class, $userLesson);
+            $form->handleRequest($request);
+            $entityManager->remove($userLesson);
+            $entityManager->flush();
+        }
+
         return $this->redirectToRoute('app_lessons', ['date' => date('Y-m-d')]);
     }
 }
